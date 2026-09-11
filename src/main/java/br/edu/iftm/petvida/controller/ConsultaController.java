93ca83b0f8a2908df72ddfb2c0ff97c564bb57c2
lazaro00junio <lazaro.araujo@estudante.iftm.edu.br>
2026-09-10 22:48:56 -0300

    parte-d: defeitos corrigidos

--- src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
+++ src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
@@ -1,19 +1,20 @@
 package br.edu.iftm.petvida.controller;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import br.edu.iftm.petvida.repository.AnimalRepository;
- 
-public class ConsultaController {                          // <== ITEM 1
- 
+
+@Controller
+public class ConsultaController {
+
     @Autowired
     AnimalRepository repository;
- 
-    @GetMapping("consulta")                                // <== ITEM 2
+
+    @GetMapping("consulta")
     public String consulta(Model model) {
-        model.addAttribute("bicho", repository.buscarPorId(1));   // <== ITEM 3
-        return "consulta.html";                            // <== ITEM 4
+        model.addAttribute("animal", repository.buscarPorId(178)); 
+        return "consulta"; 
     }
 }
